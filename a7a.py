0add35c7f8a1b6e8bdfce19b7cd7c6d701095018
yassin <yabdelgawad@csss.es>
2023-02-07 22:30:50 +0100

    first commit

---
+++ a7a.py
@@ -0,0 +1,6 @@
+def fahrenheit_to_celsius():
+  fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+  celsius = (fahrenheit - 32) * 5/9
+  print("The temperature in Celsius is:", celsius)
+
+fahrenheit_to_celsius()
--- yassin.py
+++ yassin.py
@@ -1,19 +1,8 @@
-
-matrix = [
-    [True, 34, 5, 'School'],
-    [8, 23, 20, 11, 37, 55, 17],
-    ['book', 'mosh', 'arc', 'photo'],
-    [-1, 'Mani']
-]
-
-for i in matrix[2]:
-    print(i.upper())
-
-for num in reversed(matrix[1]):
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
-           
+print("Welcome")
+print("Enter your length of the rectangle")
+Length=float(input())
+print("Enter your width of the rectangle")
+Width=float(input())
+area=Length*Width
+print(area)
+print("thank you")
